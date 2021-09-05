a8413e79295d5dac121d4ce1824295d5721b31fa
devarie <schaaparie@gmail.com>
2021-09-05 14:01:12 +0200

    api workin and refreshing on pageg

--- src/Waltjr.tsx
+++ src/Waltjr.tsx
@@ -18,7 +18,6 @@ const Waltjr = () => {
         console.log(result.data);
 
         setItems(result.data);
-
         // omdat je altijd maar een quote terugkrijgt zou je ook misschien alleen de quote kunnen opslaan in the state. Misschien netter dus iets van setItems(result.data[0])
         // dan moet je dus ook de TS aanpassen. Ook kan je items misschien veranderen in quotes omdat duidelijker is.
     };
@@ -35,13 +34,12 @@ const Waltjr = () => {
             {/* ik heb er `items &&` voor gezet, ik weet niet of je deze notatie kent maar dit is een soort if statement. Het betekent dat het 'truthy' moet zijn om de
           quote te laten zien. (truthy betekent een soort van true haha. Dus als het niet null of undefined is dan is  het truthy) als je dit deel weggaat zie je een TS error */}
             <p>new quote:{items && items[0].quote}</p>
-            <button></button>
+            <button onClick={fetchItems}>Get new Quote</button>
         </div>
     );
 };
 
 export default Waltjr;
-
 //     return (
 //       <div className='container'>
 //         <Header />
